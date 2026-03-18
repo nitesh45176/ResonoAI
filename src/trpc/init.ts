4abc34a784c2eac50cf116ff60b32508f007eb81
Nitesh Mishra <mishranitesh45176@gmail.com>
2026-03-18 18:48:31 +0530

    bonus: sentry error monitoring

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -2,7 +2,7 @@ import { auth } from '@clerk/nextjs/server';
 import { initTRPC, TRPCError } from '@trpc/server';
 import { cache } from 'react';
 import superjson from 'superjson';
-
+import * as Sentry from "@sentry/node"
 
 export const createTRPCContext = cache(async () => {
   /**
@@ -20,13 +20,20 @@ const t = initTRPC.create({
    */
   transformer: superjson, 
 });
+
+const sentryMiddleware = t.middleware(
+  Sentry.trpcMiddleware({
+    attachRpcInput: true
+  })
+)
+
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
-export const baseProcedure = t.procedure;
+export const baseProcedure = t.procedure.use(sentryMiddleware)
 
 // Authenticated procedure - calls auth() only when needed
-export const authProcedure = t.procedure.use(async ({next}) => {
+export const authProcedure = baseProcedure.use(async ({next}) => {
   const {userId} = await auth()
 
   if(!userId){
@@ -40,7 +47,7 @@ export const authProcedure = t.procedure.use(async ({next}) => {
 
 
 // Organization procedure - requires userId and orgId
-export const orgProcedure = t.procedure.use(async ({next}) => {
+export const orgProcedure = baseProcedure.use(async ({next}) => {
   const {userId, orgId} = await auth()
 
   if(!userId){
